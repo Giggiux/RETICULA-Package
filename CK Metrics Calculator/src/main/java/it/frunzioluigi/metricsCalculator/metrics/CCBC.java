cab103107b6b23cbdda5ce5c80344d67f63c8497
Luigi Frunzio <frunzl@usi.ch>
2017-06-26 14:34:42 +0200

    updated CK Metrics calculator

--- CK Metrics Calculator/src/main/java/it/frunzioluigi/metricsCalculator/metrics/CCBC.java
+++ CK Metrics Calculator/src/main/java/it/frunzioluigi/metricsCalculator/metrics/CCBC.java
@@ -8,6 +8,7 @@ import it.frunzioluigi.metricsCalculator.utils.VSM;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 /**
  * Created by giggiux on 3/13/17.
@@ -22,39 +23,66 @@ public class CCBC {
 
 	public void calculate(Collection<CKNumber> allReports) {
 
+		//TODO: Future work - add words count as metric, so not to recompute every time in plugin. Extends CKNumber for this.
 		Map<String, Map<String, Double>> classesWordsCount = new HashMap<>(allReports.size());
 
-		for (CKNumber ck : allReports) {
-			double sum = 0.;
+		CKNumber[] cks = new CKNumber[allReports.size()];
+		cks = allReports.toArray(cks);
 
-			Map<String, Double> ck1Map = classesWordsCount.get(ck.getClassName());
+		int cksLength = cks.length;
 
-			if (ck1Map == null) {
-				ck1Map = wordsCount(ck.getFile());
-				classesWordsCount.put(ck.getClassName(), ck1Map);
-			}
+		double[] couplings = new double[cksLength];
 
+		for (int i = 0; i < cksLength; i++) {
+			CKNumber ck = cks[i];
 
-			for (CKNumber ck2 : allReports) {
-				if (ck.equals(ck2)) continue;
+			Map<String, Double> ck1Map = classesWordsCount.computeIfAbsent(ck.getClassName(), (String useless) -> {
+				return wordsCount(ck.getFile());
+			});
 
-				Map<String, Double> ck2Map = classesWordsCount.get(ck2.getClassName());
+			for (int j = i + 1; j < cksLength; j++) {
+				CKNumber ck2 = cks[j];
+				Map<String, Double> ck2Map = classesWordsCount.computeIfAbsent(ck2.getClassName(), (String useless) -> {
+					return wordsCount(ck2.getFile());
+				});
 
-				if (ck2Map == null) {
-					ck2Map = wordsCount(ck2.getFile());
-					classesWordsCount.put(ck2.getClassName(), ck2Map);
-				}
+				double vsm = VSM.calculate(ck1Map, ck2Map);
 
-				sum += VSM.calculate(ck1Map, ck2Map);
-
+				couplings[i] += vsm;
+				couplings[j] += vsm;
 			}
 
 			int size = allReports.size();
-
-			double avg = sum / (size * (size - 1) / 2);
-
+			double avg = couplings[i] / (size * (size - 1) / 2.);
 			int avgToInt = Utils.doubleToInt(avg);
+			ck.addSpecific("CCBC", avgToInt);
+		}
 
+	}
+
+	public void calculate(Map<String, CKNumber> allReports, Map<String, CKNumber> report) {
+		Map<String, Map<String, Double>> classesWordsCount = new HashMap<>(report.size());
+
+		for (CKNumber ck : report.values()) {
+			double sum = 0.;
+
+			Map<String, Double> ck1Map = classesWordsCount.computeIfAbsent(ck.getClassName(), (String useless) -> {
+				return wordsCount(ck.getFile());
+			});
+
+			for (CKNumber ck2 : allReports.values()) {
+				if (ck.getClassName().equals(ck2.getClassName())) continue;
+
+				Map<String, Double> ck2Map = classesWordsCount.computeIfAbsent(ck2.getClassName(), (String useless) -> {
+					return wordsCount(ck2.getFile());
+				});
+
+				sum += VSM.calculate(ck1Map, ck2Map);
+			}
+
+			int size = allReports.size();
+			double avg = sum / (size * (size - 1) / 2.);
+			int avgToInt = Utils.doubleToInt(avg);
 			ck.addSpecific("CCBC", avgToInt);
 		}
 
